10cbf37ca8bcb3f8bad66ce4d69dd0caac251d1f
Yuri Gonçalves <yuri_rjx@hotmail.com>
2022-09-18 19:20:39 -0300

    end of module 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { FiltroDeExcecaoHttp } from './common/filtros/filtro-de-excecao-http.filter';
 
 @Module({
     imports: [UsuarioModule],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: APP_FILTER,
+            useClass: FiltroDeExcecaoHttp,
+        },
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ClassSerializerInterceptor,
+        },
+    ],
 })
 export class AppModule {}
--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,9 +1,13 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { IsNomeDeUsuarioUnico } from './is-nome-de-usuario-unico.validator';
+import { Exclude, Expose } from 'class-transformer';
 
 export class Usuario {
     id: number;
 
+    @Expose({
+        name: 'username',
+    })
     @IsNomeDeUsuarioUnico({
         message: 'Nome de usuário precisa ser único.',
     })
@@ -15,20 +19,35 @@ export class Usuario {
     })
     nomeDeUsuario: string;
 
+    @Expose({
+        name: 'email',
+    })
     @IsEmail({
         message: 'Email inválido.',
     })
     email: string;
 
+    @Expose({
+        name: 'password',
+    })
+    @Exclude({
+        toPlainOnly: true,
+    })
     @IsNotEmpty({
         message: 'Senha é obrigatória.',
     })
     senha: string;
 
+    @Expose({
+        name: 'fullName',
+    })
     @IsNotEmpty({
         message: 'Nome completo é obrigatório.',
     })
     nomeCompleto: string;
 
+    @Expose({
+        name: 'joinDate',
+    })
     dataDeEntrada: Date;
 }
